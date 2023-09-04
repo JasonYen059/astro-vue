5e205ded703593828383a709ed14e002f12ccccb
Yen <jason753357@gmail.com>
2023-09-04 18:20:03 +0800

    install tailwind, add pages

--- src/components/HeaderNav.vue
+++ src/components/HeaderNav.vue
@@ -1,18 +1,23 @@
 <template>
-    <div>
-        {{ titleName }}
-    </div>
+    <nav class="container mx-auto flex flex-row justify-between py-5 border-b-4">
+        <a class="logo text-2xl" href="/">YEN</a>
+        <ul class="flex flex-row">
+            <li v-for="link in navList" :key="link.link" class=" text-lg px-3">
+                <a :href="link.link">{{ link.text }}</a>
+            </li>
+        </ul>
+    </nav>
 </template>
 
 <script setup>
 // import { ref } from 'vue';
 
 const props = defineProps({
-    titleName:String
+    titleName: String
 });
+const navList = [
+    { text: 'ABOUT', link: 'about' },
+    { text: 'WORK', link: 'work' },
+]
 
 </script>
-
-<style lang="scss" scoped>
-
-</style>
