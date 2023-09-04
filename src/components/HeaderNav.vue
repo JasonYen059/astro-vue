90eb05e1f25ffe7c7c52767ebe9cd2a24d559843
Yen <jason753357@gmail.com>
2023-09-04 17:35:53 +0800

    first commit

---
+++ src/components/HeaderNav.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        {{ titleName }}
+    </div>
+</template>
+
+<script setup>
+// import { ref } from 'vue';
+
+const props = defineProps({
+    titleName:String
+});
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
